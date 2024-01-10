b718676d8d1e1c75dd7f0e8fb89191f12fae4e51
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2024-01-10 14:09:00 +0200

    add

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,12 +3,19 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+     form: document.querySelector('form'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  /* refs.openModalBtn.addEventListener('click', toggleModal); */
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.form.addEventListener('submit', onValidateForm);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  };
+  function onValidateForm(evt) {
+    evt.preventDefault();
+    refs.modal.classList.toggle("is-hidden")
+    refs.form.reset();
   }
 })();
